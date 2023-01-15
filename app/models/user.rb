6106e38344583bc64468fa94661ae325fd960fac
Light <light@MacBook-Pro-Light.local>
2023-01-16 06:18:55 +0700

    Add method set_name to model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :events
 
   validates :name, presence: true, length: { maximum: 35 }
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "Товарищ №#{rand(100)}" if self.name.blank?
+  end
 end
