4d539039debe1bdfed6c63994e1dfa8a1b0f6d87
Light <light@MacBook-Pro-Light.local>
2023-01-18 09:32:11 +0700

    db reboot

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2023_01_17_123801) do
     t.string "user_name"
     t.string "user_email"
     t.bigint "event_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_subscriptions_on_event_id"
