ef2a88b679a6bd8e5ef607b033b52c491e609301
Light <light@MacBook-Pro-Light.local>
2023-01-18 09:32:01 +0700

    Fixes on comment and subscription models

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,6 @@
 class Subscription < ApplicationRecord
   belongs_to :event
-  belongs_to :user
+  belongs_to :user, optional: true
 
   validates :event, presence: true
 
@@ -8,7 +8,7 @@ class Subscription < ApplicationRecord
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
 
   validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
-  validates :email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
+  validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
 
   def user_name
     if user.present?
